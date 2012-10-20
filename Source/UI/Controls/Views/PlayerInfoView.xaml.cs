adb9d36abe0416e5cd021b2a0d0bf238d439077c
yinlin09 <yinlin09@gmail.com>
2012-10-20 02:12:58 -0400

    Equip in Playerinfoview

--- Source/UI/Controls/Views/PlayerInfoView.xaml.cs
+++ Source/UI/Controls/Views/PlayerInfoView.xaml.cs
@@ -111,5 +111,128 @@ namespace Sanguosha.UI.Controls
         {
             return base.RemoveDelayedTool(card);
         }
+
+        protected override void AddEquipment(CardView card)
+        {
+            Equipment equip = card.Card.Type as Equipment;
+
+            if (equip == null)
+            {
+                throw new ArgumentException("Cannot add non-equip to equip area.");
+            }
+
+            SmallEquipView equipLabel = new SmallEquipView();
+            equipLabel.DataContext = card.CardViewModel;
+            
+            Canvas targetArea = null;
+            switch (equip.Category)
+            {
+                case CardCategory.Weapon:
+                    targetArea = weaponArea;
+                    break;
+                case CardCategory.Armor:
+                    targetArea = armorArea;
+                    break;
+                case CardCategory.DefensiveHorse:
+                    targetArea = horse1Area;
+                    break;
+                case CardCategory.OffsensiveHorse:
+                    targetArea = horse2Area;
+                    break;
+                default:
+                    throw new ArgumentException("Cannot install non-equips to equip area.");
+            }
+            equipLabel.Opacity = 0;
+
+            if (targetArea.Children.Count != 0)
+            {
+                throw new ArgumentException("Duplicate equip not allowed.");
+            }
+            targetArea.Children.Clear();
+            targetArea.Children.Add(equipLabel);
+
+            Point dest = targetArea.TranslatePoint(new Point(targetArea.Width / 2, targetArea.Height / 2),
+                                                   ParentGameView.GlobalCanvas);
+            dest.Offset(-card.Width / 2, -card.Height / 2);
+            card.Position = dest;
+            card.CardOpacity = 0.0;
+            card.DisappearAfterMove = true;
+            card.Rebase(0.3d);
+
+            Storyboard storyBoard = new Storyboard();
+            DoubleAnimation animation1 = new DoubleAnimation();
+            DoubleAnimation animation2 = new DoubleAnimation();
+            animation1.From = -50d;
+            animation1.To = 0d;
+            animation2.To = 1d;
+            animation1.Duration = TimeSpan.FromMilliseconds(500);
+            animation2.Duration = TimeSpan.FromMilliseconds(500);
+            Storyboard.SetTarget(animation1, equipLabel);
+            Storyboard.SetTarget(animation2, equipLabel);
+            Storyboard.SetTargetProperty(animation1, new PropertyPath(Canvas.TopProperty));
+            Storyboard.SetTargetProperty(animation2, new PropertyPath(SmallEquipView.OpacityProperty));
+            storyBoard.Children.Add(animation1);
+            storyBoard.Children.Add(animation2);
+            storyBoard.Begin();
+        }
+
+        protected override CardView RemoveEquipment(Card card)
+        {
+            Equipment equip = card.Type as Equipment;
+
+            if (equip == null)
+            {
+                throw new ArgumentException("Cannot add non-equip to equip area.");
+            }
+
+            Canvas targetArea = null;
+            switch (equip.Category)
+            {
+                case CardCategory.Weapon:
+                    targetArea = weaponArea;
+                    break;
+                case CardCategory.Armor:
+                    targetArea = armorArea;
+                    break;
+                case CardCategory.DefensiveHorse:
+                    targetArea = horse1Area;
+                    break;
+                case CardCategory.OffsensiveHorse:
+                    targetArea = horse2Area;
+                    break;
+                default:
+                    throw new ArgumentException("Cannot install non-equips to equip area.");
+            }
+
+            if (targetArea.Children.Count == 0)
+            {
+                throw new ArgumentException("No equip is found.");
+            }
+
+            SmallEquipView equipLabel = targetArea.Children[0] as SmallEquipView;
+            targetArea.Children.Clear();
+
+            CardView result = CardView.CreateCard(card);
+            result.Opacity = 0;
+            Point dest = targetArea.TranslatePoint(new Point(0, 0), ParentGameView.GlobalCanvas);
+            result.Position = dest;
+            result.Rebase(0);
+
+            Storyboard storyBoard = new Storyboard();
+            DoubleAnimation animation1 = new DoubleAnimation();
+            DoubleAnimation animation2 = new DoubleAnimation();
+            animation1.To = 50d;
+            animation2.To = 0.0d;
+            animation1.Duration = TimeSpan.FromMilliseconds(500);
+            animation2.Duration = TimeSpan.FromMilliseconds(500);
+            Storyboard.SetTarget(animation1, equipLabel);
+            Storyboard.SetTarget(animation2, equipLabel);
+            Storyboard.SetTargetProperty(animation1, new PropertyPath(Canvas.TopProperty));
+            Storyboard.SetTargetProperty(animation2, new PropertyPath(SmallEquipView.OpacityProperty));
+            storyBoard.Children.Add(animation1);
+            storyBoard.Children.Add(animation2);
+            storyBoard.Begin();
+            return result;
+        }
     }
 }
