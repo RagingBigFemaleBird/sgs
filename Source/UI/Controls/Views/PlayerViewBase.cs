be1a80ee2503dd550b6933316d5ceab01300cf07
QTurtle <yinlindukechina@gmail.com>
2012-11-02 01:05:39 -0400

    Minor layout adjustment of login page.

--- Source/UI/Controls/Views/PlayerViewBase.cs
+++ Source/UI/Controls/Views/PlayerViewBase.cs
@@ -220,5 +220,11 @@ namespace Sanguosha.UI.Controls
         public virtual void Tremble()
         {
         }
+
+        protected override Geometry GetLayoutClip(Size layoutSlotSize)
+        {
+            return ClipToBounds ? base.GetLayoutClip(layoutSlotSize) : null;
+        }
+
     }
 }
