c9611abae21f720924d8afa0528256a29757ae59
QTurtle <yinlindukechina@gmail.com>
2012-11-02 01:05:39 -0400

    Minor layout adjustment of login page.

---
+++ Source/UI/Controls/Views/NonClipGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Sanguosha.UI.Controls
+{
+    public class NonClipGrid : Grid
+    {
+        protected override Geometry GetLayoutClip(Size layoutSlotSize)
+        {
+            return ClipToBounds ? base.GetLayoutClip(layoutSlotSize) : null;
+        }
+
+    }
+}
