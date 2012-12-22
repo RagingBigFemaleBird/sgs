66878ef9eb24087b11f8066aa1257749b3894e57
yinlin09 <yinlin09@gmail.com>
2012-12-22 15:05:43 -0500

    Ruler given skills.

--- Source/UI/Controls/ViewModels/PlayerViewModel.cs
+++ Source/UI/Controls/ViewModels/PlayerViewModel.cs
@@ -178,6 +178,7 @@ namespace Sanguosha.UI.Controls
                 {
                     AutoInvokeSkillCommands.Remove(skillCommand);
                     DockedSkillCommands.Remove(skillCommand);
+                    RulerGivenSkillCommands.Remove(skillCommand);
                 }
             }
 
