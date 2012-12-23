5af33c88d68c9f6cb5e53463e9f10dac609b036a
QTurtle <yinlindukechina@gmail.com>
2012-12-23 13:54:14 -0500

    Fix xiangle

--- Source/Expansions/Hills/Skills/XiangLe.cs
+++ Source/Expansions/Hills/Skills/XiangLe.cs
@@ -36,6 +36,8 @@ namespace Sanguosha.Expansions.Hills.Skills
             }
         }
 
+        private static CardAttribute xiangLeAttribute = CardAttribute.Register("XiangLe");
+
         public void OnPlayerIsCardTarget(Player owner, GameEvent gameEvent, GameEventArgs eventArgs)
         {
             ISkill skill;
@@ -46,7 +48,7 @@ namespace Sanguosha.Expansions.Hills.Skills
                 Game.CurrentGame.HandleCardDiscard(eventArgs.Source, cards);
                 return;
             }
-            eventArgs.ReadonlyCard[CardAttribute.Register("XiangLe" + owner.Id)] = 1;
+            eventArgs.ReadonlyCard[xiangLeAttribute] = 1;
         }
 
         public XiangLe()
@@ -59,7 +61,7 @@ namespace Sanguosha.Expansions.Hills.Skills
             ) { Priority = SkillPriority.XiangLe };
             var trigger2 = new AutoNotifyPassiveSkillTrigger(
                 this,
-                (p, e, a) => { return a.ReadonlyCard != null && a.ReadonlyCard[CardAttribute.Register("XiangLe" + p.Id)] == 1; },
+                (p, e, a) => { return a.ReadonlyCard != null && a.ReadonlyCard[xiangLeAttribute] == 1; },
                 (p, e, a) => { throw new TriggerResultException(TriggerResult.End); },
                 TriggerCondition.OwnerIsTarget
             );
