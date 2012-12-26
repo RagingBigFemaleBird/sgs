e8182faee7e2cb2930fb28c28a1d97ef8e9e75af
Bi <biwu85@hotmail.com>
2012-12-25 20:32:14 -0500

    DuanChang. Expect bugs.

---
+++ Source/Expansions/Hills/Skills/DuanChang.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sanguosha.Core.Triggers;
+using Sanguosha.Core.Cards;
+using Sanguosha.Core.UI;
+using Sanguosha.Core.Skills;
+using Sanguosha.Expansions.Battle.Cards;
+using Sanguosha.Core.Players;
+using Sanguosha.Core.Games;
+using Sanguosha.Core.Heroes;
+
+namespace Sanguosha.Expansions.Hills.Skills
+{
+    /// <summary>
+    /// 断肠-锁定技，杀死你的角色失去当前的所有武将技能。
+    /// </summary>
+    public class DuanChang : TriggerSkill
+    {
+        public DuanChang()
+        {
+            var trigger = new AutoNotifyPassiveSkillTrigger(
+                this,
+                (p, e, a) => { return a.Source != null; },
+                (p, e, a) =>
+                {
+                    var h = a.Source.Hero;
+                    foreach (ISkill sk in new List<ISkill>(a.Source.AdditionalSkills))
+                    {
+                        a.Source.LoseAdditionalSkill(sk);
+                    }
+                    foreach (var sk in a.Source.Hero.Skills)
+                    {
+                        sk.Owner = null;
+                    }
+                    a.Source.Hero = new Hero(h.Name, h.IsMale, h.Allegiance, h.MaxHealth, new List<ISkill>());
+                },
+                TriggerCondition.OwnerIsTarget
+            );
+
+            Triggers.Add(GameEvent.PlayerIsDead, trigger);
+            IsEnforced = true;
+        }
+    }
+}
