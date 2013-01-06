b7700c448b6cb601264c8888a3cc6b00f3eb3ad7
Ellis <ellis_loveless@qq.com>
2013-01-06 18:02:02 +0800

    ShangShi Fixed

--- Source/Expansions/OverKnightFame11/Skills/ShangShi.cs
+++ Source/Expansions/OverKnightFame11/Skills/ShangShi.cs
@@ -23,15 +23,29 @@ namespace Sanguosha.Expansions.OverKnightFame11.Skills
         {
             var trigger = new AutoNotifyPassiveSkillTrigger(
                 this,
-                (p, e, a) => 
+                (p, e, a) =>
                 {
-                    if (e == GameEvent.AfterHealthChanged && p.Health - (a as HealthChangedEventArgs).Delta < 0)
+                    if (e != GameEvent.AfterHealthChanged && a.Source != p)
                         return false;
-                    return (Game.CurrentGame.CurrentPhaseEventIndex == 3 || Game.CurrentGame.CurrentPhase != TurnPhase.Discard) && p.HandCards().Count < p.LostHealth; 
+                    if (e == GameEvent.CardsLost)
+                    {
+                        var result = from c in a.Cards select c.HistoryPlace1;
+                        if (!result.Any(d => d.DeckType == DeckType.Hand))
+                            return false;
+                    }
+                    if (e == GameEvent.CardsAcquired)
+                    {
+                        var result = from c in a.Cards select c.Place;
+                        if (!result.Any(d => d.DeckType == DeckType.Hand))
+                            return false;
+                    }
+                    if (e == GameEvent.AfterHealthChanged && (p.Health - (a as HealthChangedEventArgs).Delta < 0 || !a.Targets.Contains(p)))
+                        return false;
+                    return (Game.CurrentGame.CurrentPhaseEventIndex == 3 || Game.CurrentGame.CurrentPhase != TurnPhase.Discard) && p.HandCards().Count < p.LostHealth;
                 },
                 (p, e, a) => { Game.CurrentGame.DrawCards(p, p.LostHealth - p.HandCards().Count); },
-                TriggerCondition.OwnerIsSource
-            );
+                TriggerCondition.Global
+            ) { Priority = int.MinValue};
             Triggers.Add(GameEvent.CardsLost, trigger);
             Triggers.Add(GameEvent.CardsAcquired, trigger);
             Triggers.Add(GameEvent.AfterHealthChanged, trigger);
