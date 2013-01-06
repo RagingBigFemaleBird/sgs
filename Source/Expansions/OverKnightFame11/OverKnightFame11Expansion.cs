86103b4c8867de98ad9223df27f892722142b826
Bi <biwu85@hotmail.com>
2013-01-05 21:29:40 -0500

    ShangShi. Expect bugs...

--- Source/Expansions/OverKnightFame11/OverKnightFame11Expansion.cs
+++ Source/Expansions/OverKnightFame11/OverKnightFame11Expansion.cs
@@ -28,7 +28,7 @@ namespace Sanguosha.Expansions.OverKnightFame11
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("XuSheng", true, Allegiance.Wu, 4, new PoJun()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("XuShu", true, Allegiance.Shu, 3, new WuYan(), new JuJian()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("YuJin", true, Allegiance.Wei, 4, new YiZhong()))));
-            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ZhangChunhua", false, Allegiance.Wei, 3, new JueQing()/*, new ShangShi()*/))));
+            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ZhangChunhua", false, Allegiance.Wei, 3, new JueQing(), new ShangShi()))));
         }
     }
 }
---
+++ Source/Expansions/OverKnightFame11/Skills/ShangShi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sanguosha.Core.UI;
+using Sanguosha.Core.Skills;
+using Sanguosha.Core.Players;
+using Sanguosha.Core.Games;
+using Sanguosha.Core.Triggers;
+using Sanguosha.Core.Exceptions;
+using Sanguosha.Core.Cards;
+using System.Diagnostics;
+using Sanguosha.Expansions.Basic.Cards;
+
+namespace Sanguosha.Expansions.OverKnightFame11.Skills
+{
+    /// <summary>
+    /// 伤逝-除弃牌阶段外，每当你的手牌数小于你已损失的体力值时，可立即将手牌数补至等同于你已损失的体力值。
+    /// </summary>
+    public class ShangShi : TriggerSkill
+    {
+        public ShangShi()
+        {
+            var trigger = new AutoNotifyPassiveSkillTrigger(
+                this,
+                (p, e, a) => { return Game.CurrentGame.CurrentPhase != TurnPhase.Discard && p.HandCards().Count < p.MaxHealth - p.Health; },
+                (p, e, a) => { Game.CurrentGame.DrawCards(p, p.MaxHealth - p.Health - p.HandCards().Count); },
+                TriggerCondition.OwnerIsSource
+            );
+            Triggers.Add(GameEvent.CardsLost, trigger);
+            Triggers.Add(GameEvent.CardsAcquired, trigger);
+            Triggers.Add(GameEvent.AfterHealthChanged, trigger);
+            Triggers.Add(GameEvent.PhaseBeginEvents[TurnPhase.End], trigger);
+            Triggers.Add(GameEvent.PhasePostEnd, trigger);
+            IsAutoInvoked = true;
+        }
+    }
+}
