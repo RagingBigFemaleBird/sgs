e967375c60831feec3660fda25329c7206cc46b3
Ellis <ellis_loveless@qq.com>
2013-01-06 23:14:19 +0800

    Format

--- Source/Expansions/SP/Skills/HuXiao.cs
+++ Source/Expansions/SP/Skills/HuXiao.cs
@@ -16,20 +16,20 @@ using Sanguosha.Expansions.Basic.Cards;
 namespace Sanguosha.Expansions.SP.Skills
 {
     /// <summary>    
-	/// 【虎啸】——若你于出牌阶段使用的【杀】被【闪】抵消，则本阶段你可以额外使用一张【杀】。
-	/// </summary>
+    /// 虎啸-若你于出牌阶段使用的【杀】被【闪】抵消，则本阶段你可以额外使用一张【杀】。
+    /// </summary>
     public class HuXiao : TriggerSkill
     {
-	    public HuXiao()
-	    {
-	    var trigger = new AutoNotifyPassiveSkillTrigger(
-	        this,
-	        (p, e, a) => { return Game.CurrentGame.CurrentPhase == TurnPhase.Play && Game.CurrentGame.CurrentPlayer == p;},
-	        (p, e, a) => { p[Sha.AdditionalShaUsable]++; },
-	        TriggerCondition.OwnerIsSource
-	        ) { AskForConfirmation = false };
-	    Triggers.Add(ShaCancelling.PlayerShaTargetDodged, trigger);
-	    IsAutoInvoked = null;
-	    }
-	}
+        public HuXiao()
+        {
+            var trigger = new AutoNotifyPassiveSkillTrigger(
+                this,
+                (p, e, a) => { return Game.CurrentGame.CurrentPhase == TurnPhase.Play && Game.CurrentGame.CurrentPlayer == p; },
+                (p, e, a) => { p[Sha.AdditionalShaUsable]++; },
+                TriggerCondition.OwnerIsSource
+                ) { AskForConfirmation = false };
+            Triggers.Add(ShaCancelling.PlayerShaTargetDodged, trigger);
+            IsAutoInvoked = null;
+        }
+    }
 }
