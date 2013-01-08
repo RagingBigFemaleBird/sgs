9cb0fff152a02d1c3992558138fc3543558a020a
Ellis <ellis_loveless@qq.com>
2013-01-08 23:16:59 +0800

    HuLaoLvBu2

---
+++ Source/Expansions/SP/Skills/ShenWei.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sanguosha.Core.Triggers;
+using Sanguosha.Core.UI;
+using Sanguosha.Core.Skills;
+using Sanguosha.Core.Games;
+using Sanguosha.Core.Players;
+using Sanguosha.Core.Exceptions;
+
+namespace Sanguosha.Expansions.SP.Skills
+{
+    /// <summary>
+    /// 神威-锁定技，摸牌阶段，你额外摸两张牌；你的手牌上限+2。
+    /// </summary>
+    public class ShenWei : TriggerSkill
+    {
+        public ShenWei()
+        {
+            var trigger = new AutoNotifyPassiveSkillTrigger(
+                this,
+                (p, e, a) => { p[Player.DealAdjustment] += 2; },
+                TriggerCondition.OwnerIsSource
+            );
+            Triggers.Add(GameEvent.PhaseProceedEvents[TurnPhase.Draw], trigger);
+            var trigger2 = new AutoNotifyPassiveSkillTrigger(
+                 this,
+                 (p, e, a) =>
+                 {
+                     var args = a as AdjustmentEventArgs;
+                     args.AdjustmentAmount += 2;
+                 },
+                 TriggerCondition.OwnerIsSource
+             ) { IsAutoNotify = false};
+            Triggers.Add(GameEvent.PlayerHandCardCapacityAdjustment, trigger2);
+            IsEnforced = true;
+        }
+    }
+}
---
+++ Source/Expansions/SP/Skills/XiuLuo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using Sanguosha.Core.Triggers;
+using Sanguosha.Core.Cards;
+using Sanguosha.Core.UI;
+using Sanguosha.Core.Skills;
+using Sanguosha.Expansions.Basic.Cards;
+using Sanguosha.Core.Games;
+using Sanguosha.Core.Players;
+using Sanguosha.Core.Exceptions;
+
+namespace Sanguosha.Expansions.SP.Skills
+{
+    /// <summary>
+    /// 修罗-回合开始阶段开始时，你可以弃置与你判定区里一张牌相同花色的一张手牌，然后你弃置你判定区里的该牌。
+    /// </summary>
+    public class XiuLuo : TriggerSkill
+    {
+        class XiuLuoVerifier : ICardChoiceVerifier
+        {
+            public VerifierResult Verify(List<List<Card>> answer)
+            {
+                if ((answer != null && answer.Count > 2) ||
+                    (answer != null && (answer[0] != null && answer[0].Count > 1 || answer[1] != null && answer[0].Count > 1)))
+                {
+                    return VerifierResult.Fail;
+                }
+                if (answer != null && answer.Count > 0)
+                {
+                    if (answer[0] != null && answer[0].Count != 0 && answer[0][0].Place.DeckType == DeckType.Hand)
+                    {
+                        return VerifierResult.Fail;
+                    }
+                    if (answer[1] != null && answer[1].Count != 0 && answer[1][0].Place.DeckType == DeckType.DelayedTools)
+                    {
+                        return VerifierResult.Fail;
+                    }
+                }
+                if (answer[0] == null || answer[0].Count == 0 || answer[1] == null || answer[1].Count == 0)
+                {
+                    return VerifierResult.Partial;
+                }
+                Card fromDelayed = answer[0][0];
+                Card fromHand = answer[1][0];
+                if (fromHand != null && fromDelayed != null && fromHand.Suit != fromDelayed.Suit)
+                {
+                    return VerifierResult.Fail;
+                }
+                return VerifierResult.Success;
+            }
+            public UiHelper Helper
+            {
+                get { return null; }
+            }
+        }
+
+        void Run(Player owner, GameEvent gameEvent, GameEventArgs args)
+        {
+            while (owner.DelayedTools().Count > 0 && owner.HandCards().Count > 0 && AskForSkillUse())
+            {
+                List<List<Card>> answer;
+                List<DeckPlace> sourceDeck = new List<DeckPlace>();
+                sourceDeck.Add(new DeckPlace(owner, DeckType.DelayedTools));
+                sourceDeck.Add(new DeckPlace(owner, DeckType.Hand));
+                if (owner.AskForCardChoice(new CardChoicePrompt("XiuLuo", owner),
+                    sourceDeck,
+                    new List<string>() { "XLJinNang", "XLShouPai" },
+                    new List<int>() { 1, 1 },
+                    new XiuLuoVerifier(),
+                    out answer,
+                    null,
+                    CardChoiceCallback.GenericCardChoiceCallback))
+                {
+                    NotifySkillUse();
+                    Game.CurrentGame.HandleCardDiscard(owner, answer[1]);
+                    Game.CurrentGame.HandleCardDiscard(owner, answer[0]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public XiuLuo()
+        {
+            var trigger = new AutoNotifyPassiveSkillTrigger(
+                this,
+                (p, e, a) => { return p.DelayedTools().Count > 0 && p.HandCards().Count > 0; },
+                Run,
+                TriggerCondition.OwnerIsSource
+            ) { AskForConfirmation = false, IsAutoNotify = false };
+            Triggers.Add(GameEvent.PhaseBeginEvents[TurnPhase.Start], trigger);
+            IsAutoInvoked = null;
+        }
+    }
+}
