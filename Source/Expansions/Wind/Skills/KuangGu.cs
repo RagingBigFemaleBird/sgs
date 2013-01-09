17dec1164b13950069343aa360f5b3ed2932cb5a
Slob <xindonger@gmail.com>
2013-01-09 20:59:56 +0800

    KuangGu fixes

--- Source/Expansions/Wind/Skills/KuangGu.cs
+++ Source/Expansions/Wind/Skills/KuangGu.cs
@@ -29,14 +29,21 @@ namespace Sanguosha.Expansions.Wind.Skills
         {
             var trigger = new AutoNotifyPassiveSkillTrigger(
                 this,
-                (p, e, a) => { return p.LostHealth > 0 &&  Game.CurrentGame.DistanceTo(p, a.Targets[0]) <= 1; },
+                (p, e, a) => { return p.LostHealth > 0 && p[KuangGuUsable] == 1; },
                 Run,
                 TriggerCondition.OwnerIsSource
             );
-
             Triggers.Add(GameEvent.AfterDamageCaused, trigger);
+            var trigger2 = new AutoNotifyPassiveSkillTrigger(
+                this,
+                (p, e, a) => { return Game.CurrentGame.DistanceTo(p, a.Targets[0]) <= 1 && a.Source[KuangGuUsable] == 0; },
+                (p, e, a) => { a.Source[KuangGuUsable] = 1; },
+                TriggerCondition.Global
+            ) { IsAutoNotify = false, AskForConfirmation = false, Priority = int.MinValue };
+            Triggers.Add(GameEvent.DamageInflicted, trigger2);
             IsEnforced = true;
         }
 
+        private static PlayerAttribute KuangGuUsable = PlayerAttribute.Register("KuangGuUsable");
     }
 }
