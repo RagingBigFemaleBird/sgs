22b5268691e55ff422927c88cd838cb299ecdb7d
yinlin09 <yinlin09@gmail.com>
2013-01-09 20:24:19 -0500

    Create database.

---
+++ Source/Lobby/Server/AccountContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using Sanguosha.Lobby.Core;
+
+namespace Sanguosha.Lobby.Server
+{
+    class AccountContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
