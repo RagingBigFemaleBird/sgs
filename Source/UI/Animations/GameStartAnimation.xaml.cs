2d13cf8e518dff7968645176cb54fd5b54917daf
yinlin09 <yinlin09@gmail.com>
2013-01-11 17:33:53 -0500

    Some missing files.

---
+++ Source/UI/Animations/GameStartAnimation.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sanguosha.UI.Animations
+{
+    /// <summary>
+    /// Interaction logic for PinDianLoseAnimation.xaml
+    /// </summary>
+    public partial class GameStartAnimation : AnimationBase
+    {        
+        public GameStartAnimation()
+        {
+            this.InitializeComponent();
+        }     
+    }
+}
