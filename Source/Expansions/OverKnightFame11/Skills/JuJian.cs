e25ba85385b01b719cb8fefb890c3fff6d1658b2
Ellis <ellis_loveless@qq.com>
2013-01-18 18:46:35 +0800

    new XuShu

--- Source/Expansions/OverKnightFame11/Skills/JuJian.cs
+++ Source/Expansions/OverKnightFame11/Skills/JuJian.cs
@@ -15,50 +15,79 @@ using Sanguosha.Core.Players;
 namespace Sanguosha.Expansions.OverKnightFame11.Skills
 {
     /// <summary>
-    /// 举荐-出牌阶段，你可以弃至多三张牌，然后让一名其他角色摸等量的牌。若你以此法弃牌不少于三张且均为同一类别，你回复1点体力。每回合限一次。
+    /// 举荐-回合结束阶段开始时，你可以弃置一张非基本牌，令一名其他角色选择一项：摸两张牌，或回复1点体力，或将其武将牌翻至正面朝上并重置之。
     /// </summary>
-    public class JuJian : AutoVerifiedActiveSkill
+    public class JuJian : TriggerSkill
     {
-        public override bool Commit(GameEventArgs arg)
+        class JuJianVerifier : CardsAndTargetsVerifier
         {
-            Owner[JuJianUsed] = 1;
-            int count = arg.Cards.Count;
-            Trace.Assert(count <= 3);
-            int typeCount = (from c in arg.Cards 
-                             select c.Type.BaseCategory()).Distinct().Count();
-            Game.CurrentGame.HandleCardDiscard(Owner, arg.Cards);
-            Game.CurrentGame.DrawCards(arg.Targets[0], count);
-            if (typeCount == 1 && count == 3)
+            public JuJianVerifier()
             {
-                Game.CurrentGame.RecoverHealth(Owner, Owner, 1);
+                MaxCards = 1;
+                MinCards = 1;
+                MaxPlayers = 1;
+                MinPlayers = 1;
+                Discarding = true;
             }
-            return true;
+
+            protected override bool VerifyCard(Player source, Card card)
+            {
+                return !card.Type.IsCardCategory(CardCategory.Basic);
+            }
+
+            protected override bool VerifyPlayer(Player source, Player player)
+            {
+                return player != source;
+            }
         }
 
+        void Run(Player Owner, GameEvent gameEvent, GameEventArgs eventArgs, List<Card> cards, List<Player> players)
+        {
+            Game.CurrentGame.HandleCardDiscard(Owner, cards);
+            Player target = players[0];
+            List<OptionPrompt> options = new List<OptionPrompt>();
+            options.Add(new OptionPrompt("JuJianMoPai"));
+            if (target.LostHealth > 0)
+            {
+                options.Add(new OptionPrompt("JuJianHuiXue"));
+            }
+            if (target.IsImprisoned || target.IsIronShackled)
+            {
+                options.Add(new OptionPrompt("JuJianChongZhi"));
+            }
+            int answer = 0;
+            if (options.Count > 1)
+            {
+                target.AskForMultipleChoice(new MultipleChoicePrompt("JuJian"), options, out answer);
+            }
+            if (answer == 0)
+            {
+                Game.CurrentGame.DrawCards(target, 2);
+                return;
+            }
+            else if (answer == 1)
+            {
+                if (target.LostHealth > 0)
+                {
+                    Game.CurrentGame.RecoverHealth(Owner, target, 1);
+                    return;
+                }
+
+            }
+            target.IsImprisoned = false;
+            target.IsIronShackled = false;
+        }
 
         public JuJian()
         {
-            MinCards = 1;
-            MaxCards = 3;
-            MaxPlayers = 1;
-            MinPlayers = 1;
-            Discarding = true;
+            var trigger = new AutoNotifyUsagePassiveSkillTrigger(
+                this,
+                Run,
+                TriggerCondition.OwnerIsSource,
+                new JuJianVerifier()
+            );
+            Triggers.Add(GameEvent.PhaseBeginEvents[TurnPhase.End], trigger);
+            IsAutoInvoked = null;
         }
-        public static PlayerAttribute JuJianUsed = PlayerAttribute.Register("JuJianUsed", true);
-        protected override bool? AdditionalVerify(Player source, List<Card> cards, List<Player> players)
-        {
-            return source[JuJianUsed] == 0;
-        }
-
-        protected override bool VerifyCard(Player source, Card card)
-        {
-            return true;
-        }
-
-        protected override bool VerifyPlayer(Player source, Player player)
-        {
-            return source != player;
-        }
-
     }
 }
--- Source/Expansions/OverKnightFame11/Skills/WuYan.cs
+++ Source/Expansions/OverKnightFame11/Skills/WuYan.cs
@@ -16,7 +16,7 @@ using Sanguosha.Core.Exceptions;
 namespace Sanguosha.Expansions.OverKnightFame11.Skills
 {
     /// <summary>
-    /// 无言-锁定技，你使用的非延时类锦囊对其他角色无效；其他角色使用的非延时类锦囊对你无效。
+    /// 无言-锁定技，你防止你造成或受到的任何锦囊牌的伤害。
     /// </summary>
     public class WuYan : TriggerSkill
     {
@@ -24,17 +24,16 @@ namespace Sanguosha.Expansions.OverKnightFame11.Skills
         {
             var trigger = new AutoNotifyPassiveSkillTrigger(
                 this,
-                (p, e, a) => 
+                (p, e, a) =>
                 {
-                    return ((a.Source == p && a.Targets[0] != p) || (a.Source != p && a.Targets[0] == p)) &&
-                             a.ReadonlyCard.Type.IsCardCategory(CardCategory.ImmediateTool);
+                    return (a.Source == p && e == GameEvent.DamageCaused || a.Targets[0] == p && e == GameEvent.DamageInflicted) && a.ReadonlyCard.Type.IsCardCategory(CardCategory.Tool);
                 },
                 (p, e, a) => { throw new TriggerResultException(TriggerResult.End); },
                 TriggerCondition.Global
             );
-            Triggers.Add(GameEvent.CardUsageTargetValidating, trigger);
+            Triggers.Add(GameEvent.DamageCaused, trigger);
+            Triggers.Add(GameEvent.DamageInflicted, trigger);
             IsEnforced = true;
         }
-
     }
 }
