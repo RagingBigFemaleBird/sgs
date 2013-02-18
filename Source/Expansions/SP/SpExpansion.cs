d0a9663a5a160990fe19a5be6c68aaf820056f53
Ellis <334236579@qq.com>
2013-02-18 16:46:42 +0800

    Sp LingJu

--- Source/Expansions/SP/SpExpansion.cs
+++ Source/Expansions/SP/SpExpansion.cs
@@ -14,6 +14,7 @@ using Sanguosha.Expansions.Fire.Skills;
 using Sanguosha.Expansions.Woods.Skills;
 using Sanguosha.Expansions.Hills.Skills;
 using Sanguosha.Expansions.Basic.Skills;
+using Sanguosha.Expansions.Assassin.Skills;
 
 namespace Sanguosha.Expansions.SP
 {
@@ -39,6 +40,7 @@ namespace Sanguosha.Expansions.SP
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("GuanYinping", false, Allegiance.Shu, 3, new XueJi(), new HuXiao(), new WuJi()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ChenLin", true, Allegiance.Wei, 3, new BiFa(), new SongCi()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("XiahouBa", true, Allegiance.Shu, 4, new BaoBian()))));
+            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("QunLingJu", true, Allegiance.Qun, 3, new JieYuan(), new FenXin()) { HeroConvertFrom = "LingJu" })));
         }
     }
 }
