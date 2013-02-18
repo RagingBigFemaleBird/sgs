93df9fb7ff3511327e4e525ce55d6a5e554a9594
Ellis <334236579@qq.com>
2013-02-18 23:26:35 +0800

    typo

--- Source/Expansions/OverKnightFame11/OverKnightFame11Expansion.cs
+++ Source/Expansions/OverKnightFame11/OverKnightFame11Expansion.cs
@@ -19,7 +19,7 @@ namespace Sanguosha.Expansions.OverKnightFame11
             CardSet = new List<Card>();
 
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("CaoZhi", true, Allegiance.Wei, 3, new LuoYing(), new JiuShi()))));
-            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ChenGong", true, Allegiance.Wei, 3, new MingCe(), new ZhiChi()))));
+            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ChenGong", true, Allegiance.Qun, 3, new MingCe(), new ZhiChi()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("FaZheng", true, Allegiance.Shu, 3, new EnYuan(), new XuanHuo()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("GaoShun", true, Allegiance.Qun, 4, new XianZhen(), new JinJiu()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("LingTong", true, Allegiance.Wu, 4, new XuanFeng()))));
