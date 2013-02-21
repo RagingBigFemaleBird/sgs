b9d9605fc0ea4d029fb221632c1603fce3469217
Ellis <334236579@qq.com>
2013-02-21 15:08:14 +0800

    fixed

--- Source/UI/Controls/Views/Containers/CardStack.cs
+++ Source/UI/Controls/Views/Containers/CardStack.cs
@@ -239,7 +239,7 @@ namespace Sanguosha.UI.Controls
                 card.SetValue(Canvas.ZIndexProperty, zindex + i);
                 card.AddRebaseAnimation(sb, 0.4d);
             }
-            sb.DecelerationRatio = 0.1d;
+            sb.DecelerationRatio = 0.6d;
             sb.Begin(this, HandoffBehavior.Compose);
         }
 
