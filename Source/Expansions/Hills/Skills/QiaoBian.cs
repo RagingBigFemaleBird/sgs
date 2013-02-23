d06f0333331bb849b39525b7e302ab3a536232cb
Ellis <334236579@qq.com>
2013-02-23 16:12:40 +0800

    QiaoBian fixed

--- Source/Expansions/Hills/Skills/QiaoBian.cs
+++ Source/Expansions/Hills/Skills/QiaoBian.cs
@@ -28,15 +28,10 @@ namespace Sanguosha.Expansions.Hills.Skills
         {
             public DrawVerifier()
             {
-                MinCards = 1;
-                MaxCards = 1;
-                MinPlayers = 0;
+                MaxCards = 0;
+                MinPlayers = 1;
                 MaxPlayers = 2;
                 Discarding = true;
-            }
-            protected override bool VerifyCard(Player source, Card card)
-            {
-                return card.Place.DeckType == DeckType.Hand;
             }
             protected override bool VerifyPlayer(Player source, Player player)
             {
@@ -48,29 +43,50 @@ namespace Sanguosha.Expansions.Hills.Skills
         {
             public MoveVerifier()
             {
-                MinCards = 1;
-                MaxCards = 1;
+                MaxCards = 0;
                 MinPlayers = 2;
                 MaxPlayers = 2;
                 Discarding = true;
-            }
-            protected override bool VerifyCard(Player source, Card card)
-            {
-                return card.Place.DeckType == DeckType.Hand;
             }
             protected override bool? AdditionalVerify(Player source, List<Card> cards, List<Player> players)
             {
                 if (players != null && players.Count == 2)
                 {
-                    bool hasSomethingToExchange = false;
-                    foreach (var p in players)
+                    if (players.All(p => p.DelayedTools().Count == 0 && p.Equipments().Count == 0))
                     {
-                        if (Game.CurrentGame.Decks[p, DeckType.DelayedTools].Count != 0 || Game.CurrentGame.Decks[p, DeckType.Equipment].Count != 0)
+                        return false;
+                    }
+                    bool hasSomethingToExchange;
+                    bool delayToolsCheck = false;
+                    bool equipmentCheck = false;
+                    if (!players.Any(p => p.DelayedTools().Count == 0) && players[0].DelayedTools().Count == players[1].DelayedTools().Count)
+                    {
+                        foreach (var tool in players[0].DelayedTools())
                         {
-                            hasSomethingToExchange = true;
+                            if ((tool.Type as DelayedTool).DelayedToolConflicting(players[1]))
+                            {
+                                continue;
+                            }
+                            delayToolsCheck = true;
+                            break;
                         }
                     }
-                    if (!hasSomethingToExchange) return false;
+                    else delayToolsCheck = !players.All(p => p.DelayedTools().Count == 0);
+                    if (delayToolsCheck == false && !players.Any(p => p.Equipments().Count == 0) && players[0].Equipments().Count == players[1].Equipments().Count)
+                    {
+                        for (int i = 0; i < players[0].Equipments().Count; i++)
+                        {
+                            if (players[0].Equipments()[i].Type.IsCardCategory(players[1].Equipments()[i].Type.Category))
+                            {
+                                continue;
+                            }
+                            equipmentCheck = true;
+                            break;
+                        }
+                    }
+                    else equipmentCheck = true;
+                    hasSomethingToExchange = delayToolsCheck || equipmentCheck;
+                    return hasSomethingToExchange;
                 }
                 return true;
             }
@@ -95,10 +111,13 @@ namespace Sanguosha.Expansions.Hills.Skills
 
         protected void Draw(Player player, GameEvent gameEvent, GameEventArgs eventArgs, List<Card> cards, List<Player> players)
         {
-            Game.CurrentGame.HandleCardDiscard(player, cards);
-            Game.CurrentGame.EnterAtomicContext();
+            Game.CurrentGame.SortByOrderOfComputation(Game.CurrentGame.CurrentPlayer, players);
+            StagingDeckType QiaoBianDeck = new StagingDeckType("QiaoBian");
+            CardsMovement move = new CardsMovement();
+            move.Helper.IsFakedMove = true;
             foreach (Player p in players)
             {
+                if (p.HandCards().Count == 0) continue;
                 List<List<Card>> answer;
                 var places = new List<DeckPlace>() { new DeckPlace(p, DeckType.Hand) };
                 if (!Game.CurrentGame.UiProxies[Owner].AskForCardChoice(new CardChoicePrompt("QiaoBian", Owner), places,
@@ -107,13 +126,22 @@ namespace Sanguosha.Expansions.Hills.Skills
                     answer = new List<List<Card>>();
                     answer.Add(Game.CurrentGame.PickDefaultCardsFrom(places));
                 }
-                Game.CurrentGame.HandleCardTransferToHand(p, Owner, answer[0]);
+                move.Cards = answer[0];
+                move.To = new DeckPlace(p, QiaoBianDeck);
+                Game.CurrentGame.MoveCards(move, false, Core.Utils.GameDelayTypes.None);
+                Game.CurrentGame.PlayerLostCard(p, answer[0]);
             }
-            Game.CurrentGame.ExitAtomicContext();
-            Game.CurrentGame.CurrentPhase++;
-            Game.CurrentGame.CurrentPhaseEventIndex = 2;
+            move.Cards.Clear();
+            move.Helper.IsFakedMove = false;
+            move.To = new DeckPlace(Owner, DeckType.Hand);
+            foreach (Player p in players)
+            {
+                move.Cards.AddRange(Game.CurrentGame.Decks[p, QiaoBianDeck]);
+            }
+            cards = new List<Card>(move.Cards);
+            Game.CurrentGame.MoveCards(move);
+            Game.CurrentGame.PlayerAcquiredCard(Owner, cards);
             Game.CurrentGame.NotificationProxy.NotifyActionComplete();
-            throw new TriggerResultException(TriggerResult.End);
         }
 
         class QiaoBianMoveVerifier : ICardChoiceVerifier
@@ -211,45 +239,54 @@ namespace Sanguosha.Expansions.Hills.Skills
                 {
                     Game.CurrentGame.HandleCardTransfer(dest, source, targetDeck, new List<Card>() { theCard });
                 }
-                
-            }
 
-            Game.CurrentGame.CurrentPhase++;
-            Game.CurrentGame.CurrentPhaseEventIndex = 2;
-            throw new TriggerResultException(TriggerResult.End);
+            }
+            Game.CurrentGame.NotificationProxy.NotifyActionComplete();
         }
 
+        Prompt GetPrompt()
+        {
+            return new CardUsagePrompt(string.Format("{0}.{1}", this.GetType().Name, GenerateSpecialEffectHintIndex(Owner, null) + 1), this);
+        }
 
         public QiaoBian()
         {
-            var trigger = new AutoNotifyUsagePassiveSkillTrigger(
+            var trigger = new AutoNotifyPassiveSkillTrigger(
                 this,
-                Draw,
-                TriggerCondition.OwnerIsSource,
-                new DrawVerifier()
-            );
-            var trigger2 = new AutoNotifyUsagePassiveSkillTrigger(
-                this,
-                (p, e, a, cards, players) =>
+                (p, e, a) =>
                 {
-                    Game.CurrentGame.HandleCardDiscard(p, cards);
-                    Game.CurrentGame.CurrentPhase++;
-                    Game.CurrentGame.CurrentPhaseEventIndex = 2;
-                    throw new TriggerResultException(TriggerResult.End);
+                    ISkill skill;
+                    List<Card> cards;
+                    List<Player> players;
+                    if (p.AskForCardUsage(GetPrompt(), new SkipOnlyVerifier(), out skill, out cards, out players))
+                    {
+                        NotifySkillUse(players);
+                        Game.CurrentGame.HandleCardDiscard(p, cards);
+                        if (Game.CurrentGame.CurrentPhase == TurnPhase.Judge)
+                        {
+                            if (p.AskForCardUsage(new CardUsagePrompt("QiaoBianDraw"), new DrawVerifier(), out skill, out cards, out players))
+                            {
+                                Draw(p, e, a, cards, players);
+                            }
+                        }
+                        else if (Game.CurrentGame.CurrentPhase == TurnPhase.Draw)
+                        {
+                            if (p.AskForCardUsage(new CardUsagePrompt("QiaoBianMove"), new MoveVerifier(), out skill, out cards, out players))
+                            {
+                                Move(p, e, a, cards, players);
+                            }
+                        }
+                        Game.CurrentGame.CurrentPhase++;
+                        Game.CurrentGame.CurrentPhaseEventIndex = 2;
+                        throw new TriggerResultException(TriggerResult.End);
+                    }
                 },
-                TriggerCondition.OwnerIsSource,
-                new SkipOnlyVerifier()
-            );
-            var trigger3 = new AutoNotifyUsagePassiveSkillTrigger(
-                this,
-                Move,
-                TriggerCondition.OwnerIsSource,
-                new MoveVerifier()
-            );
+                TriggerCondition.OwnerIsSource
+            ) { AskForConfirmation = false, IsAutoNotify = false };
             Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Judge], trigger);
-            Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Draw], trigger3);
-            Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Start], trigger2);
-            Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Play], trigger2);
+            Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Draw], trigger);
+            Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Start], trigger);
+            Triggers.Add(GameEvent.PhaseOutEvents[TurnPhase.Play], trigger);
             IsAutoInvoked = null;
         }
     }
