77e64795bf4b8cf713dacf1a2e7815630c84e124
Ellis <334236579@qq.com>
2013-03-01 18:48:24 +0800

    Protocol Version update

--- Source/Lobby/Service/Misc.cs
+++ Source/Lobby/Service/Misc.cs
@@ -7,7 +7,7 @@ namespace Sanguosha.Lobby.Core
 {
     public class Misc
     {
-        public static int ProtocolVersion = 12;
+        public static int ProtocolVersion = 16;
         public static int MaxChatLength = 160;
     }
 }
