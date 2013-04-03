8e42cd59e650f5f5dd22227ad7b94566e1e733bc
Ellis <334236579@qq.com>
2013-04-03 16:41:59 +0800

    ManCong

--- Source/Expansions/OverKnightFame13/OverKnightFame13Expansion.cs
+++ Source/Expansions/OverKnightFame13/OverKnightFame13Expansion.cs
@@ -23,6 +23,7 @@ namespace OverKnightFame12
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("LiRu", true, Allegiance.Qun, 3, new JueCe(), new MieJi(), new FenCheng()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("LiuFeng", true, Allegiance.Shu, 4, new XianSi()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("GuoHuai", true, Allegiance.Wei, 4, new JingCe()))));
+            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ManChong", true, Allegiance.Wei, 3, new JunXing(), new YuCe()))));
         }
     }
 }
