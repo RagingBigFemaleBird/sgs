e56d72c12ad293fdd1e4c6ad3986cd3f817f619b
Bi <biwu85@gmail.com>
2013-04-08 21:12:39 -0400

    QiaoShui, not working yet.

--- Source/Expansions/OverKnightFame13/OverKnightFame13Expansion.cs
+++ Source/Expansions/OverKnightFame13/OverKnightFame13Expansion.cs
@@ -22,7 +22,7 @@ namespace OverKnightFame12
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("GuanPing", true, Allegiance.Shu, 4, new LongYin()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("LiRu", true, Allegiance.Qun, 3, new JueCe(), new MieJi(), new FenCheng()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("LiuFeng", true, Allegiance.Shu, 4, new XianSi()))));
-            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("PanZhangMaZhong", true, Allegiance.Wu, 4, new DuoDao(), new AnJian()))));
+            CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("PanZhangMaZhong", true, Allegiance.Wu, 4, new QiaoShui(), new AnJian()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("GuoHuai", true, Allegiance.Wei, 4, new JingCe()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("ManChong", true, Allegiance.Wei, 3, new JunXing(), new YuCe()))));
             CardSet.Add(new Card(SuitType.None, -1, new HeroCardHandler(new Hero("CaoChong", true, Allegiance.Wei, 3, new ChengXiang(), new BingXin()))));
