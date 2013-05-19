61224b943f984e0fbcce438b39d148cf0add7aaa
RagingBigFemaleBird <biwu85@gmail.com>
2013-05-19 14:01:34 -0400

    AoCai, just an empty file.

---
+++ Source/Expansions/SP/Skills/AoCai.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using Sanguosha.Core.Triggers;
+using Sanguosha.Core.Cards;
+using Sanguosha.Core.UI;
+using Sanguosha.Core.Skills;
+using Sanguosha.Expansions.Basic.Cards;
+using Sanguosha.Core.Games;
+using Sanguosha.Core.Players;
+using Sanguosha.Core.Exceptions;
+
+namespace Sanguosha.Expansions.SP.Skills
+{
+    public class AoCai : CardTransformSkill
+    {
+        public override VerifierResult TryTransform(List<Card> cards, List<Player> arg, out CompositeCard card, bool isPlay)
+        {
+            card = new CompositeCard();
+            card.Subcards = new List<Card>();
+            card.Type = null;
+            if (cards != null && cards.Count > 0)
+            {
+                return VerifierResult.Fail;
+            }
+            return VerifierResult.Success;
+        }
+
+        public AoCai()
+        {
+        }
+
+        public CardHandler AdditionalType { get; set; }
+    }
+}
